11f3c83a6b5f2f45a5d683bc1c0b7e9aea32699b
Joe Grodecki <lanceandjoe@gmail.com>
2014-05-09 22:32:15 -0400

    The beginnings

---
+++ src/com/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController {
+ 
+	@RequestMapping(value="/welcome")
+	public String welcome() {
+		return "welcome";
+	}
+}
